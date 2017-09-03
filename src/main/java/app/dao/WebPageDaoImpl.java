20d39c815da83850bac666deadf7e0a908508c92
YevheniiSemenov <semenovevgenij92@gmail.com>
2017-09-03 12:55:03 +0300

    Save web page in db

--- src/main/java/app/dao/WebPageDaoImpl.java
+++ src/main/java/app/dao/WebPageDaoImpl.java
@@ -2,14 +2,20 @@ package app.dao;
 
 import app.model.WebPage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.UncategorizedSQLException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import javax.sql.DataSource;
+import java.util.List;
+
+import java.util.logging.Logger;
 
 @Component
 public class WebPageDaoImpl implements WebPageDAO {
 
     private JdbcTemplate jdbcTemplate;
+
+    private Logger logger = Logger.getLogger(this.getClass().getName());
 
     @Autowired
     public WebPageDaoImpl(final DataSource dataSource) {
@@ -18,6 +24,13 @@ public class WebPageDaoImpl implements WebPageDAO {
 
     @Override
     public synchronized void saveOrUpdate(WebPage webPage) {
+        String sql = "REPLACE INTO index_page (url, title, text) VALUES (?, ?, ?)";
+        try {
+            // TODO find solution for saving utf8mb4 text in db
+            jdbcTemplate.update(sql, webPage.getUrl(), webPage.getTitle(), webPage.getText());
+        } catch (UncategorizedSQLException e) {
+            logger.warning("Page from " + webPage.getUrl() + " url not saved in db. Throwed UncategorizedSQLException");
+        }
     }
 
     @Override
