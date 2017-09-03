bb395d8ad8101a54cbe8e98abccee5b5a55eb6f5
YevheniiSemenov <semenovevgenij92@gmail.com>
2017-09-03 10:00:18 +0300

    Create logger field in IndexProcessorTask

--- src/main/java/app/task/IndexProcessorTask.java
+++ src/main/java/app/task/IndexProcessorTask.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 
+import java.util.logging.Logger;
+
 @Component
 @Scope("prototype")
 public class IndexProcessorTask implements Runnable {
@@ -13,6 +15,9 @@ public class IndexProcessorTask implements Runnable {
 
     private volatile Set<String> urls = new HashSet<>();
     private int maxSearchUrlDeep = 1;
+
+    private Logger logger = Logger.getLogger(this.getClass().getName());
+
     @Override
     public void run() {
         indexProcess(maxSearchUrlDeep);
