c6968f532a3b21bb7401ac4c532131614c2fe622
YevheniiSemenov <semenovevgenij92@gmail.com>
2017-09-03 09:51:06 +0300

    Add urls, maxSearchUrlDeep fields and setters

--- src/main/java/app/task/IndexProcessorTask.java
+++ src/main/java/app/task/IndexProcessorTask.java
@@ -7,8 +7,18 @@ import org.springframework.stereotype.Component;
 @Scope("prototype")
 public class IndexProcessorTask implements Runnable {
 
+    private volatile Set<String> urls = new HashSet<>();
+    private int maxSearchUrlDeep = 1;
     @Override
     public void run() {
+    public void setUrls(Set<String> urls) {
+        this.urls = urls;
+    }
+
+    public void setMaxSearchUrlDeep(int maxSearchUrlDeep) {
+        this.maxSearchUrlDeep = maxSearchUrlDeep;
+    }
+
     }
 
 }
