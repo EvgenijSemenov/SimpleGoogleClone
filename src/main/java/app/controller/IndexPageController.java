15aac08cb0d3136e8b7d3160127d9ab49ca7cc25
YevheniiSemenov <semenovevgenij92@gmail.com>
2017-09-03 09:34:04 +0300

    Add mapping for post method

--- src/main/java/app/controller/IndexPageController.java
+++ src/main/java/app/controller/IndexPageController.java
@@ -1,10 +1,12 @@
 package app.controller;
 
 import app.service.WebPageServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestParam;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @Controller
 public class IndexPageController {
@@ -15,6 +17,13 @@ public class IndexPageController {
     @RequestMapping(path = "/index", method=GET)
     public String indexPage() {
         return "index";
+    }
+
+    @RequestMapping(path = "/index", method=POST)
+    public String indexingPage(@RequestParam(value="q") String url) {
+        webPageService.indexByUrl(url, 2);
+
+        return "indexResult";
     }
 
 }
