9898a5caae51a1df14b8d95c5a101cd5ff2c49be
YevheniiSemenov <semenovevgenij92@gmail.com>
2017-09-03 21:05:08 +0300

    Create search method in SearchController

--- src/main/java/app/controller/SearchController.java
+++ src/main/java/app/controller/SearchController.java
@@ -1,16 +1,31 @@
 package app.controller;
 
+import app.service.WebPageServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @Controller
 public class SearchController {
 
+    @Autowired
+    WebPageServiceImpl webPageService;
 
     @RequestMapping(path = "/", method=GET)
     public String index() {
         return "index";
     }
+
+    @RequestMapping(path = "/search", method=GET)
+    public ModelAndView search(@RequestParam(value="q") String searchText) {
+        ModelAndView model = new ModelAndView("search");
+        model.addObject("searchResultList", webPageService.fulltextSearch(searchText));
+
+        return model;
+    }
+
 }
