32f8d95c590e0224026a8028fc931b7e062ba3ba
YevheniiSemenov <semenovevgenij92@gmail.com>
2017-09-03 22:12:29 +0300

    Get properties for jdbc from application.properties

--- src/main/java/app/config/DbConfig.java
+++ src/main/java/app/config/DbConfig.java
@@ -1,20 +1,25 @@
 package app.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import javax.sql.DataSource;
 
 @Configuration
 public class DbConfig {
 
+    @Autowired
+    private Environment env;
+
     @Bean
     public DataSource getDataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/google_clone?useUnicode=true&characterEncoding=UTF-8");
-        dataSource.setUsername("root");
-        dataSource.setPassword("123456");
+        dataSource.setDriverClassName(env.getRequiredProperty("jdbc.driverClassName"));
+        dataSource.setUrl(env.getRequiredProperty("jdbc.url"));
+        dataSource.setUsername(env.getRequiredProperty("jdbc.username"));
+        dataSource.setPassword(env.getRequiredProperty("jdbc.password"));
 
         return dataSource;
     }
