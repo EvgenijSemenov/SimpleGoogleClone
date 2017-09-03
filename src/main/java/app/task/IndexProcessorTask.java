8f36301da698a1c233b4c4ca4134a8c65dd57647
YevheniiSemenov <semenovevgenij92@gmail.com>
2017-09-03 09:42:31 +0300

    Create IndexProcessorTask class

---
+++ src/main/java/app/task/IndexProcessorTask.java
@@ -0,0 +1,10 @@
+package app.task;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("prototype")
+public class IndexProcessorTask implements Runnable {
+
+}
