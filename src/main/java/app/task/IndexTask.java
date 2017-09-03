884f081f81491a8661bea3e0d46391b73de1480c
YevheniiSemenov <semenovevgenij92@gmail.com>
2017-09-03 11:47:31 +0300

    Create loadDocument method

--- src/main/java/app/task/IndexTask.java
+++ src/main/java/app/task/IndexTask.java
@@ -1,5 +1,11 @@
 package app.task;
 
+import org.jsoup.Connection.Response;
+import org.jsoup.HttpStatusException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
@@ -9,6 +15,24 @@ public class IndexTask implements Runnable {
 
     @Override
     public void run() {
+        Response response = loadDocument(url);
+    }
+
+    private Response loadDocument(String url) {
+        Response response = null;
+
+        try {
+            response = Jsoup.connect(url).execute();
+        } catch (HttpStatusException e) {
+            logger.info(url);
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return response;
+    }
+
     }
 
 }
