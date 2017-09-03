20e8042bba42b6f104e451d22df99ecf30295d64
YevheniiSemenov <semenovevgenij92@gmail.com>
2017-09-03 09:48:34 +0300

    Inject IndexProcessorTask in WebPageServiceImpl and execute indexProcessorTask in indexByUrl method

--- src/main/java/app/service/WebPageServiceImpl.java
+++ src/main/java/app/service/WebPageServiceImpl.java
@@ -1,5 +1,7 @@
 package app.service;
 
+import app.task.IndexProcessorTask;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.stereotype.Service;
 
@@ -9,8 +11,13 @@ public class WebPageServiceImpl implements WebPageService {
     @Autowired
     TaskExecutor taskExecutor;
 
+    @Autowired
+    IndexProcessorTask indexProcessorTask;
+
     @Override
     public String indexByUrl(String indexUrl, int maxUrlsSearchDeep) {
+        taskExecutor.execute(indexProcessorTask);
+
         return null;
     }
 
