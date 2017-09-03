8bf02a5217744ea46a287bf115eebda5d906566c
YevheniiSemenov <semenovevgenij92@gmail.com>
2017-09-03 09:21:44 +0300

    Add indexByUrl override method in WebPageServiceImpl

--- src/main/java/app/service/WebPageServiceImpl.java
+++ src/main/java/app/service/WebPageServiceImpl.java
@@ -5,4 +5,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class WebPageServiceImpl implements WebPageService {
 
+    @Override
+    public String indexByUrl(String indexUrl, int maxUrlsSearchDeep) {
+        return null;
+    }
+
 }
