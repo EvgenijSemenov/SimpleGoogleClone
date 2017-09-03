fa9530e739195c08b1acb3ee1112c1160a632203
YevheniiSemenov <semenovevgenij92@gmail.com>
2017-09-03 12:38:36 +0300

    Greate data source been

--- src/main/java/app/config/DbConfig.java
+++ src/main/java/app/config/DbConfig.java
@@ -1,8 +1,22 @@
 package app.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import javax.sql.DataSource;
 
 @Configuration
 public class DbConfig {
+
+    @Bean
+    public DataSource getDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/google_clone?useUnicode=true&characterEncoding=UTF-8");
+        dataSource.setUsername("root");
+        dataSource.setPassword("123456");
+
+        return dataSource;
+    }
 
 }
