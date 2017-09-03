2502020f75821666bd6268785b7b9f3c302278a7
YevheniiSemenov <semenovevgenij92@gmail.com>
2017-09-03 12:51:43 +0300

    Initialize jdbcTemplate in WebPageDaoImpl constructor

--- src/main/java/app/dao/WebPageDaoImpl.java
+++ src/main/java/app/dao/WebPageDaoImpl.java
@@ -1,9 +1,19 @@
 package app.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+import javax.sql.DataSource;
 
 @Component
 public class WebPageDaoImpl implements WebPageDAO {
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public WebPageDaoImpl(final DataSource dataSource) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
 
     @Override
     public List<WebPage> fullTextSearch(String text) {
