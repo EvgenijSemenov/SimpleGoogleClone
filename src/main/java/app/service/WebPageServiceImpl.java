92a6c07352859bc9ec3c0d794c37e2c345021435
YevheniiSemenov <semenovevgenij92@gmail.com>
2017-09-03 09:28:06 +0300

    Inject task executor in WebPageServiceImpl class

--- src/main/java/app/service/WebPageServiceImpl.java
+++ src/main/java/app/service/WebPageServiceImpl.java
@@ -1,9 +1,13 @@
 package app.service;
 
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WebPageServiceImpl implements WebPageService {
+
+    @Autowired
+    TaskExecutor taskExecutor;
 
     @Override
     public String indexByUrl(String indexUrl, int maxUrlsSearchDeep) {
