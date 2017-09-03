cda81e46581eee8d32340b1c08cc6f4ca4b28de3
YevheniiSemenov <semenovevgenij92@gmail.com>
2017-09-03 09:26:55 +0300

    Describe TaskExecutor been in SpringTaskExecutorConfig

--- src/main/java/app/config/SpringTaskExecutorConfig.java
+++ src/main/java/app/config/SpringTaskExecutorConfig.java
@@ -1,8 +1,24 @@
 package app.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 public class SpringTaskExecutorConfig {
+
+    @Value( "${max.index.task.thread.count}" )
+    private int maxIndexTaskThreadCount;
+
+    @Bean
+    public TaskExecutor indexTaskExecutor() {
+        ThreadPoolTaskExecutor t = new ThreadPoolTaskExecutor();
+        t.setCorePoolSize(maxIndexTaskThreadCount);
+        t.setMaxPoolSize(100);
+        t.afterPropertiesSet();
+        return t;
+    }
 
 }
