e30dee679ff64cf4c810dcb3721b3d44432fdfef
YevheniiSemenov <semenovevgenij92@gmail.com>
2017-09-03 09:56:33 +0300

    Cteate indexProcess and call it in run method

--- src/main/java/app/task/IndexProcessorTask.java
+++ src/main/java/app/task/IndexProcessorTask.java
@@ -11,12 +11,18 @@ public class IndexProcessorTask implements Runnable {
     private int maxSearchUrlDeep = 1;
     @Override
     public void run() {
+        indexProcess(maxSearchUrlDeep);
+    }
+
     public void setUrls(Set<String> urls) {
         this.urls = urls;
     }
 
     public void setMaxSearchUrlDeep(int maxSearchUrlDeep) {
         this.maxSearchUrlDeep = maxSearchUrlDeep;
+    }
+
+    public void indexProcess(int maxSearchUrlDeep) {
     }
 
     }
