ac63477735b84da0b4e60226c6caf5415991bebc
YevheniiSemenov <semenovevgenij92@gmail.com>
2017-09-04 20:08:01 +0300

    Implement RowMapper interface and create override mapRow method

--- src/main/java/app/dao/mapper/WebPageRowMapper.java
+++ src/main/java/app/dao/mapper/WebPageRowMapper.java
@@ -1,4 +1,23 @@
 package app.dao.mapper;
 
-public class WebPageRowMapper {
+import app.model.WebPage;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class WebPageRowMapper implements RowMapper<WebPage> {
+
+    @Override
+    public WebPage mapRow(ResultSet rs, int rowNum) throws SQLException {
+        WebPage webPage = new WebPage();
+
+        webPage.setId(rs.getLong("id"));
+        webPage.setUrl(rs.getString("url"));
+        webPage.setTitle(rs.getString("title"));
+        webPage.setText(rs.getString("text"));
+
+        return webPage;
+    }
+
 }
