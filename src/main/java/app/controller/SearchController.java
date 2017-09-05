6cd96a20c99aabbcadebd5c4a44c6a67135547e9
YevheniiSemenov <semenovevgenij92@gmail.com>
2017-09-05 08:02:40 +0300

    Edit search method in SearchController to return search result in ModelAndView object

--- src/main/java/app/controller/SearchController.java
+++ src/main/java/app/controller/SearchController.java
@@ -1,11 +1,13 @@
 package app.controller;
 
 import app.service.WebPageService;
+import app.sql.model.SearchResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
@@ -15,15 +17,21 @@ public class SearchController {
     @Autowired
     WebPageService webPageService;
 
+
     @RequestMapping(path = "/", method=GET)
     public String index() {
         return "index";
     }
 
     @RequestMapping(path = "/search", method=GET)
-    public ModelAndView search(@RequestParam(value="q") String searchText) {
+    public ModelAndView search(@RequestParam(value="q") String searchText,
+                               @RequestParam(value="start", required = false, defaultValue = "1") int startResultNumber) {
         ModelAndView model = new ModelAndView("search");
-        model.addObject("searchResultList", webPageService.fulltextSearch(searchText));
+        SearchResult searchResult = webPageService.fulltextSearch(searchText, startResultNumber);
+
+        model.addObject("searchText", searchResult.getSearchText());
+        model.addObject("searchResultList", searchResult.getWebPageList());
+        model.addObject("searchResultCount", searchResult.getCount());
 
         return model;
     }
