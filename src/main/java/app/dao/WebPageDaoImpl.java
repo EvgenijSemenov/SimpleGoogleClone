a480169d6e6923431d1af04db0af8802b4c0bb60
YevheniiSemenov <semenovevgenij92@gmail.com>
2017-09-05 08:00:20 +0300

    Create fullTextSearchResult and fullTextSearchResultCount

--- src/main/java/app/dao/WebPageDaoImpl.java
+++ src/main/java/app/dao/WebPageDaoImpl.java
@@ -1,19 +1,18 @@
 package app.dao;
 
-import app.dao.mapper.WebPageRowMapper;
+import app.sql.mapper.CountMapper;
+import app.sql.mapper.WebPageRowMapper;
 import app.sql.SQLQuery;
 import app.sql.builder.WebPageSqlQueryBuilder;
 import app.model.WebPage;
+import app.sql.model.SearchResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.UncategorizedSQLException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 import java.util.logging.Logger;
@@ -46,10 +45,23 @@ public class WebPageDaoImpl implements WebPageDao {
     }
 
     @Override
-    public List<WebPage> fullTextSearch(String text) {
-        SQLQuery query = webPageSqlQueryBuilder.fullTextSearch(text);
+    public SearchResult fullTextSearch(String searchText, int startResultNumber) {
+        List<WebPage> webPageList = fullTextSearchResult(searchText, startResultNumber);
+        int resultCount = fullTextSearchResultCount(searchText);
+
+        return new SearchResult(searchText, webPageList, resultCount);
+    }
+
+    private List<WebPage> fullTextSearchResult(String searchText, int startResultNumber) {
+        SQLQuery query = webPageSqlQueryBuilder.fullTextSearch(searchText, startResultNumber);
 
         return jdbcTemplate.query(query.getQuery(), query.getArgs(), new WebPageRowMapper());
+    }
+
+    private int fullTextSearchResultCount(String searchText) {
+        SQLQuery query = webPageSqlQueryBuilder.fullTextSearchCount(searchText);
+
+        return jdbcTemplate.queryForObject(query.getQuery(), query.getArgs(), new CountMapper());
     }
 
 }
